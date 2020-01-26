730642a5452951172fe9fd6d313301f352c381bb
Kevinson Mwangi <knmwangi4@gmail.com>
2020-01-26 08:49:31 +0300

    Add files via upload

---
+++ gganimate.R
@@ -0,0 +1,19 @@
+
+## Loading Libraries
+library(ggplot2)
+library(gapminder)
+library(gganimate)
+library(gifski)
+library(dplyr)
+
+
+plot1 <- ggplot(gapminder, aes(x = gdpPercap, y = lifeExp, size = pop, colour=continent)) +
+  geom_point() +
+  scale_x_log10()+ 
+  transition_time(year) + 
+  labs(title = "Year: {frame_time}", x=" GDP per Capita", y="Life Expectancy") + 
+  facet_wrap(~continent) +
+  theme_bw()
+
+animate(plot1, nframes = 200, renderer = gifski_renderer("gganim.gif"))
+
